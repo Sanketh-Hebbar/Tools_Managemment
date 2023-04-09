bec0c6b6e977d472fb311bbbff24fd1c88c8a64e
Sanketh-Hebbar <sankethebbar10@gmail.com>
2023-04-09 10:19:36 +0530

    Master tools quantity gets updated after notification approval

--- src/main/java/com/interns/toolManagement/Repository/MasterRepo.java
+++ src/main/java/com/interns/toolManagement/Repository/MasterRepo.java
@@ -3,9 +3,11 @@ package com.interns.toolManagement.Repository;
 import com.interns.toolManagement.Entity.Master;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MasterRepo extends JpaRepository<Master, Long> {
-
+    @Query("SELECT m.quantity FROM Master m WHERE m.toolId = :toolId")
+    int findQuantityByToolId(@Param("toolId") Long toolId);
 }
